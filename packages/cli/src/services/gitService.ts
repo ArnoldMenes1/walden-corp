b26a9f6945a0aaf40e575d17061b530c7ccedd03
Arnold | Nexiora <menesmva@gmail.com>
2026-09-24 09:45:50 +0100

    Update gitService.ts

--- packages/cli/src/services/gitService.ts
+++ packages/cli/src/services/gitService.ts
@@ -1,1 +1,11 @@
+import { execa } from 'execa';
 
+export async function initGit(target: string): Promise<void> {
+  try {
+    await execa('git', ['init'], { cwd: target });
+    await execa('git', ['add', '.'], { cwd: target });
+    await execa('git', ['commit', '-m', 'chore: initial commit'], { cwd: target });
+  } catch {
+    // Silencieux si git n'est pas disponible
+  }
+}
