60148ca9b394fe78244931f51617f3e7492039ba
Arnold | Nexiora <menesmva@gmail.com>
2026-09-24 09:48:20 +0100

    Update errors.ts

--- packages/cli/src/utils/errors.ts
+++ packages/cli/src/utils/errors.ts
@@ -1,1 +1,10 @@
-
+export class WaldenError extends Error {
+  constructor(
+    message: string,
+    public code: string = 'UNKNOWN',
+    public suggestion?: string
+  ) {
+    super(message);
+    this.name = 'WaldenError';
+  }
+}
