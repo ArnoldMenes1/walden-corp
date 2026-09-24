27279988d25d45a045b0d7a18d802528a5edca84
Arnold | Nexiora <menesmva@gmail.com>
2026-09-24 09:48:45 +0100

    Update config.ts

--- packages/cli/src/types/config.ts
+++ packages/cli/src/types/config.ts
@@ -1,1 +1,12 @@
+export interface WaldenConfig {
+  dsVersion: string;
+  tokensPath: string;
+  figmaSyncPath?: string;
+}
 
+export interface WaldenProject {
+  name: string;
+  template: 'react' | 'vue' | 'flutter';
+  git: boolean;
+  install: boolean;
+}
